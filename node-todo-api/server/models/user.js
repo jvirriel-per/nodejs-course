05053887ad77f3c9e37624526269e16f93492022
Jose Virriel <JVirriel@intelix.ad>
2018-07-27 13:19:05 -0400

    Testing POST/ user and GET /users/me

--- node-todo-api/server/models/user.js
+++ node-todo-api/server/models/user.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
-   email: {
+  email: {
     type: String,
     required: true,
     trim: true,
@@ -32,7 +33,7 @@ var UserSchema = new mongoose.Schema({
   }]
 });
 
-UserSchema.methods.toJSON = () {
+UserSchema.methods.toJSON = function () {
   var user = this;
   var userObject = user.toObject();
 
@@ -42,30 +43,46 @@ UserSchema.methods.toJSON = () {
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
   var access = 'auth';
-  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123ps').toString();
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
 
-  user.tokens = user.tokens.concat([{access, token}]);
+  user.tokens.push({access, token});
 
-  return user.save().then(() =>{
+  return user.save().then(() => {
     return token;
   });
 };
 
-UserSchema.statics.findByToken = function (token){
+UserSchema.statics.findByToken = function (token) {
   var User = this;
   var decoded;
 
   try {
-    decoded =jwt.verify(token, 'abc123ps');
-  }catch (e){  
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
     return Promise.reject();
   }
+
   return User.findOne({
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
   });
 };
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
 var User = mongoose.model('User', UserSchema);
 
--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -103,16 +103,16 @@ app.post('/users', (req, res) => {
   var user = new User(body);
 
   user.save().then(() => {
-   return user.generateAuthToken();
-  }).then((token) =>{
-  	res.header('x-auth', token).send(user);
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   })
 });
 
-app.get('/users/me', authenticate, (req, res) =>{
-	res.send(req.user);
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
