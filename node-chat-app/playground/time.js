43e75e088c5bb326787a711635cb023e009fc483
Jose Virriel <JVirriel@intelix.ad>
2018-08-07 14:02:08 -0400

    Format timestamps using momentsjs

---
+++ node-chat-app/playground/time.js
@@ -0,0 +1,9 @@
+var moment = require('moment');
+
+//var date = moment();
+//date.add(1, 'years')
+//console.log(date.format('MMM Do, YYYY'))
+
+
+var date = moment();
+console.log(date.format('MMM Do, YYYY h:mm A'))
